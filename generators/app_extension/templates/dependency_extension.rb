a2270da1545e523a3a18a423b71967b59d328561
Todd Willey <todd@lapex.(none)>
2008-09-21 12:54:41 -0400

    to_prepare can take a name arugment, so give it.  irb now prepares properly.

--- generators/app_extension/templates/dependency_extension.rb
+++ generators/app_extension/templates/dependency_extension.rb
@@ -4,12 +4,7 @@ module <%= class_name %>DependencyExtension
     kls.class_eval do
       def register_<%= file_name %>_extension &blk
         if blk
-          ActionController::Dispatcher.to_prepare &blk
-
-          # ActionController::Dispatcher.new($stdout).prepare_application(true)
-          # is called on reset! in script/console, but is not prepared
-          # when the application is loaded for some reason, so do it manually
-          blk.call if caller.last =~ /irb/
+          ActionController::Dispatcher.to_prepare :<%= file_name %>, &blk
         end
       end
     end
