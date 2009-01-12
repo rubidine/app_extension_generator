c16ba54b54a75216bd7424a6e458504941aa9e32
Todd Willey <todd@lapex.(none)>
2009-01-12 14:17:17 -0500

    Fix up generated apps unload callbacks.

--- generators/app_extension/templates/dependency_extension.rb
+++ generators/app_extension/templates/dependency_extension.rb
@@ -17,12 +17,13 @@ module <%= class_name %>Dependencies
     return unless blk
     ActionController::Dispatcher.class_eval do
       # This is what to_prepare does, but for the after_dispatch callback
+      @after_dispatch_callbacks ||= ActiveSupport::Callbacks::CallbackChain.new
       callback = ActiveSupport::Callbacks::Callback.new(
                    :after_dispatch,
                    blk,
                    :identifier => :<%= file_name %>
                  )
-      @after_dispatch_callbacks | callback
+      @after_dispatch_callbacks.replace_or_append!(callback)
     end
   end
 end
